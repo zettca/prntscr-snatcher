af6afb8e4e5f409ced7c2cf3400201f08d63e30e
Bruno Henriques <corruption1337@gmail.com>
2015-10-04 01:09:24 +0000

    updated package.json & grammar

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # Lightshot image snatcher
 
-[Lightshot](https://app.prntscr.com/en/) lets you print-screen and automatically upload images to the internet. The uploaded images' links are sequential, as follows:
-
-- http://prnt.sc/abcde0
-- http://prnt.sc/abcde1
-- http://prnt.sc/abcde2
-- http://prnt.sc/abcde3
-- and so on...
-
-This web app exploits the fact the links are sequencial, searching for each link's image and displaying them accordingly.
+This Node.js web app exploits [Lightshot](https://app.prntscr.com/en/)'s sequential link generation to display pseudo-random uploaded images.
--- client/scripts.js
+++ client/scripts.js
@@ -3,70 +3,60 @@ var holder = document.getElementById("holder");
 loadMoreFigures();
 
 function loadMoreFigures(){
-	var chars = "0123456789abcdefghijklmnopqrstuvwxyz";
-	var prefix = getRandSet();
-	for (var i=0; i<chars.length; i++){
-		var fig = document.createElement("figure");
-		fig.imgCode = prefix + chars[i];
-		fig.innerHTML = prefix + chars[i];
-		fig.style.backgroundImage = "url(http://i.imgur.com/WZ4nhRB.gif)";
-		
-		holder.appendChild(fig);
-		getImage(fig.imgCode, fig);
-	}
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+  var prefix = getImgSet();
+  for (var i=0; i<chars.length; i++){
+    var fig = document.createElement("figure");
+    fig.imgCode = prefix + chars[i];
+    fig.style.backgroundImage = "url(http://i.imgur.com/WZ4nhRB.gif)";
+    
+    holder.appendChild(fig);
+    getImage(fig.imgCode, fig);
+  }
 }
 
 function getImage(req, ele){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function(){
     if (xhttp.readyState == 4 && xhttp.status == 200){
-    	var url = xhttp.responseText;
-    	ele.style.backgroundImage = "url("+url+")";
-    	ele.imgUrl = url;
-			ele.onclick = function(){ window.open(url, '_blank'); };
+      var url = xhttp.responseText;
+      ele.style.backgroundImage = "url("+url+")";
+      ele.imgUrl = url;
+      ele.onclick = function(){ window.open(url, '_blank'); };
     }
   };
   xhttp.open("GET", "/imgCode/" + req, true);
   xhttp.send();
 }
 
-function getRandSet(){
-	var chars = "0123456789abcdefghijklmnopqrstuvwxyz";
-	var chars1 = "0123456789abcdefghijklmn";
-	var rc = function(cs){ return cs[Math.floor(Math.random() * cs.length)];};
-	return 8 + rc(chars1)+rc(chars)+rc(chars)+rc(chars);
+function getImgSet(){
+  var chrs = "0123456789abcdefghijklmnopqrstuvwxyz";
+  var chrs2 = "0123456789abcdefghijklmn";
+  var rc = function(cs){ return cs[Math.floor(Math.random() * cs.length)];};
+  return 8 + rc(chrs2)+rc(chrs)+rc(chrs)+rc(chrs);
 }
 
-var sizes = ["64px", "96px", "128px", "192px", "256px"];
-
-function putBigger(){
-	var fig = document.styleSheets[0].cssRules[6].style;
-	var k = sizes.indexOf(fig.width)+1;
-	fig.width = sizes[k];
-	fig.height = sizes[k];
-}
-
-function putSmaller(){
-	var fig = document.styleSheets[0].cssRules[6].style;
-	var k = sizes.indexOf(fig.width)-1;
-	fig.width = sizes[k];
-	fig.height = sizes[k];
+function resizeMe(setting){
+  var fig = document.styleSheets[0].cssRules[6].style;
+  var newSize =  parseInt(fig.width, 10) + (parseInt(setting, 10) * 32);
+  fig.width = newSize + "px";
+  fig.height = newSize + "px";
 }
 
 function checkPageBottom(){
-	var currScroll = document.documentElement.scrollTop || document.body.scrollTop;
-	if (window.innerHeight+currScroll >= document.body.offsetHeight) loadMoreFigures();
+  var currScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  if (window.innerHeight+currScroll >= document.body.offsetHeight) loadMoreFigures();
 }
 
 window.onscroll = checkPageBottom;
 
 setInterval(function(){
-	var figs = document.getElementsByTagName("figure");
-	for (var i=0; i<figs.length; i++){
-		if (figs[i].imgUrl == ""){
-			figs[i].style.backgroundImage = "url(http://i.imgur.com/WZ4nhRB.gif)";
-			getImage(figs[i].imgCode, figs[i]);
-		}
-	}
-	checkPageBottom();
-}, 4000);
+  var figs = document.getElementsByTagName("figure");
+  for (var i=0; i<figs.length; i++){
+    if (figs[i].imgUrl == ""){
+      figs[i].style.backgroundImage = "url(http://i.imgur.com/WZ4nhRB.gif)";
+      getImage(figs[i].imgCode, figs[i]);
+    }
+  }
+  checkPageBottom();
+}, 3000);
