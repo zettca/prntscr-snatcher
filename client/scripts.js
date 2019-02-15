7fea273635f12a61d51e49789cd0722d0b492acc
Bruno <zettca@gmail.com>
2019-02-15 02:56:14 +0000

    Upgrade dependencies Improve client. Simplify design & Refactor js Add .gitignore

--- client/scripts.js
+++ client/scripts.js
@@ -1,62 +1,41 @@
-var holder = document.getElementById("holder");
+const root = document.getElementById('root');
+
+const CHARS = '0123456789abcdefghijklmnopqrstuvwxyz';
+const FIRST_CHAR = 'a';
 
 loadMoreFigures();
 
-function loadMoreFigures(){
-  var chars = "0123456789abcdefghijklmnopqrstuvwxyz";
-  var prefix = getImgSet();
-  for (var i=0; i<chars.length; i++){
-    var fig = document.createElement("figure");
-    fig.imgCode = prefix + chars[i];
-    fig.style.backgroundImage = "url(http://i.imgur.com/WZ4nhRB.gif)";
-    
-    holder.appendChild(fig);
-    getImage(fig.imgCode, fig);
+function loadMoreFigures() {
+  const prefix = getImgSet();
+  for (let i = 0; i < CHARS.length; i++) {
+    const code = prefix + CHARS[i];
+    const img = document.createElement('img');
+    img.src = 'http://i.imgur.com/WZ4nhRB.gif)';
+
+    root.appendChild(img);
+    getImage(code, img);
   }
 }
 
-function getImage(req, ele){
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function(){
-    if (xhttp.readyState == 4 && xhttp.status == 200){
-      var url = xhttp.responseText;
-      ele.style.backgroundImage = "url("+url+")";
-      ele.imgUrl = url;
-      ele.onclick = function(){ window.open(url, '_blank'); };
-    }
-  };
-  xhttp.open("GET", "/imgCode/" + req, true);
-  xhttp.send();
+function getImage(code, ele) {
+  fetch(`/img/${code}`)
+    .then((res) => res.text())
+    .then(url => {
+      ele.src = url;
+      ele.onclick = () => window.open(url, '_blank');
+    });
 }
 
-function getImgSet(){
-  var chrs = "0123456789abcdefghijklmnopqrstuvwxyz";
-  var chrs2 = "0123456789abcdefghijklmnopqrstuvw";
-  var rc = function(cs){ return cs[Math.floor(Math.random() * cs.length)];};
-  return "a" + rc(chrs2)+rc(chrs)+rc(chrs)+rc(chrs);
+function getImgSet() {
+  const rc = (cs) => cs[Math.floor(Math.random() * cs.length)];
+  return FIRST_CHAR + rc(CHARS) + rc(CHARS) + rc(CHARS) + rc(CHARS);
 }
 
-function resizeMe(setting){
-  var fig = document.styleSheets[0].cssRules[6].style;
-  var newSize =  parseInt(fig.width, 10) + (parseInt(setting, 10) * 32);
-  fig.width = newSize + "px";
-  fig.height = newSize + "px";
-}
-
-function checkPageBottom(){
-  var currScroll = document.documentElement.scrollTop || document.body.scrollTop;
-  if (window.innerHeight+currScroll >= document.body.offsetHeight) loadMoreFigures();
+function checkPageBottom() {
+  const currScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  if (window.innerHeight + currScroll >= document.body.offsetHeight) {
+    loadMoreFigures();
+  }
 }
 
 window.onscroll = checkPageBottom;
-
-setInterval(function(){
-  var figs = document.getElementsByTagName("figure");
-  for (var i=0; i<figs.length; i++){
-    if (figs[i].imgUrl == ""){
-      figs[i].style.backgroundImage = "url(http://i.imgur.com/WZ4nhRB.gif)";
-      getImage(figs[i].imgCode, figs[i]);
-    }
-  }
-  checkPageBottom();
-}, 2000);
--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-var express = require('express');
-var cheerio = require('cheerio');
-var cloudscraper = require('cloudscraper');
+const express = require('express');
+const cheerio = require('cheerio');
+const cloudscraper = require('cloudscraper');
 
-var app = express();
+const app = express();
 
 app.use(express.static(__dirname + '/client'));
 
-app.get("/imgCode/:id", function(req, res){
-  cloudscraper.get("http://prnt.sc/" + req.params.id, function(error, body, response){
-    if (!error){
-      res.setHeader('Content-Type', 'application/json');
-      res.send(getURLfromHTML(body));
+app.get('/img/:id', (req, res) => {
+  const { id } = req.params;
+
+  cloudscraper.get(`https://prnt.sc/${id}`, (err, resp, body) => {
+    if (err) {
+      return res.status(500).send('oopsie');
+    } else {
+      const url = getURLfromHTML(body);
+      res.send(url);
     }
   });
 });
 
-function getURLfromHTML(body){
-  var parser = cheerio.load(body);
+function getURLfromHTML(body) {
+  const parser = cheerio.load(body);
   return parser("#screenshot-image").attr("src");
 }
 
-app.listen(process.env.PORT || 3000);
+const PORT = 3000;
+app.listen(process.env.PORT || PORT, () => {
+  console.log("Server listening on port " + PORT);
+});
